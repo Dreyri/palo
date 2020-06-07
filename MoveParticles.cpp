307d6d24eb8e03e21f1d8c59437723c2387cd3a3
Frederik Engels <frederik.engels92@gmail.com>
2020-06-07 05:36:57 +0200

    Perform strength reduction

--- MoveParticles.cpp
+++ MoveParticles.cpp
@@ -1,43 +1,55 @@
 #include "main.h"
 
-void MoveParticles(const int nr_Particles, Particle* const partikel, const float dt) {
+#include <cmath>
 
-	// Schleife über alle Partikel
-	for (int i = 0; i < nr_Particles; i++) {
+void MoveParticles(const int nr_Particles, Particle *const partikel,
+                   const float dt) {
 
-		// Kraftkomponenten (x,y,z) der Kraft auf aktuellen Partikel (i) 
-		float Fx = 0, Fy = 0, Fz = 0;
+  // Schleife ï¿½ber alle Partikel
+  for (int i = 0; i < nr_Particles; i++) {
 
-		// Schleife über die anderen Partikel die Kraft auf Partikel i ausüben
-		for (int j = 0; j < nr_Particles; j++) {
+    // Kraftkomponenten (x,y,z) der Kraft auf aktuellen Partikel (i)
+    float Fx = 0, Fy = 0, Fz = 0;
 
-			// Abschwächung als zusätzlicher Abstand, um Singularität und Selbst-Interaktion zu vermeiden
-			const float softening = 1e-20;
+    // Schleife ï¿½ber die anderen Partikel die Kraft auf Partikel i ausï¿½ben
+    for (int j = 0; j < nr_Particles; j++) {
 
-			// Gravitationsgesetz
-			// Berechne Abstand der Partikel i und j
-			const float dx = partikel[j].x - partikel[i].x;
-			const float dy = partikel[j].y - partikel[i].y;
-			const float dz = partikel[j].z - partikel[i].z;
-			const float drSquared = dx * dx + dy * dy + dz * dz + softening;
-			const float drPower32 = pow(drSquared, 3.0 / 2.0);
+      // Abschwï¿½chung als zusï¿½tzlicher Abstand, um Singularitï¿½t und
+      // Selbst-Interaktion zu vermeiden
+      const float softening = 1e-20;
 
-			// Addiere Kraftkomponenten zur Netto-Kraft
-			Fx += dx / drPower32;
-			Fy += dy / drPower32;
-			Fz += dz / drPower32;
-		}
+      // Gravitationsgesetz
+      // Berechne Abstand der Partikel i und j
+      const float dx = partikel[j].x - partikel[i].x;
+      const float dy = partikel[j].y - partikel[i].y;
+      const float dz = partikel[j].z - partikel[i].z;
+      const float drSquared = dx * dx + dy * dy + dz * dz + softening;
+      // benutzung overloaded pow und exponent aendern zu float, die exponenten
+      // berechnung wird beim kompilieren rausoptimiert.
+      // hierdurch wird jetzt float pow statt double pow berechnet.
+      // const float drPower32 = std::pow(drSquared, 3.f / 2.f);
 
-		// Berechne Änderung der Geschwindigkeit des Partikel i durch einwirkende Kraft 
-		partikel[i].vx += dt * Fx;
-		partikel[i].vy += dt * Fy;
-		partikel[i].vz += dt * Fz;
-	}
+      // sqrt ist eine schnellere funktion als pow, daher haben wir den pow
+      // aufruf strength reduced zu einem sqrt aufruf.
+      float drPower32 = std::sqrt(drSquared * drSquared * drSquared);
 
-	// Bewege Partikel entsprechend der aktuellen Geschwindigkeit
-	for (int i = 0; i < nr_Particles; i++) {
-		partikel[i].x += partikel[i].vx * dt;
-		partikel[i].y += partikel[i].vy * dt;
-		partikel[i].z += partikel[i].vz * dt;
-	}
+      // Addiere Kraftkomponenten zur Netto-Kraft
+      Fx += dx / drPower32;
+      Fy += dy / drPower32;
+      Fz += dz / drPower32;
+    }
+
+    // Berechne ï¿½nderung der Geschwindigkeit des Partikel i durch einwirkende
+    // Kraft
+    partikel[i].vx += dt * Fx;
+    partikel[i].vy += dt * Fy;
+    partikel[i].vz += dt * Fz;
+  }
+
+  // Bewege Partikel entsprechend der aktuellen Geschwindigkeit
+  for (int i = 0; i < nr_Particles; i++) {
+    partikel[i].x += partikel[i].vx * dt;
+    partikel[i].y += partikel[i].vy * dt;
+    partikel[i].z += partikel[i].vz * dt;
+  }
 }
